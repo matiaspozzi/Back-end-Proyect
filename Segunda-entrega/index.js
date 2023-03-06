f5ffa1d68aeb9ccce25da39e73d3e2b6626943b4
Matias <matias_age01@hotmail.com>
2023-03-06 14:47:05 -0300

    rearmado de trabajo

--- Segunda-entrega/index.js
+++ Segunda-entrega/index.js
@@ -1,43 +1,22 @@
-import ProductsManager from "./archivos";
-const Prin = new ProductsPrin();
+import ProductManager from './desafio.js';
+const manager = new ProductManager();
 const operacionesProductos = async () => {
-try {
-    let product1 = await Prin.addProduct(
-    "1",
-    "flx4",
-    "negra",
-    100000,
-    "image",
-    50
-    );
-    console.log(product1);
-    let product2 = await Prin.addProduct(
-    "2",
-    "Cdj-2000",
-    "MARRON",
-    150000,
-    "image",
-    25
-    );
-    console.log(product2);
-    let product3 = await Prin.addProduct(
-    "3",
-    "cdj-3000",
-    "negro",
-    2000000,
-    "image",
-    25
-    );
-    console.log(product3);
+    try {
 
-    let segundaConsulta = await Prin.getProductById(2);
-    console.log(segundaConsulta);
-    let productAct = await Prin.updateProduct(1, "flx4");
-    console.log(productAct);
-    let deleteproduct1 = await Prin.deleteProducts(3);
-    console.log(deleteproduct1);
-} catch (error) {
-    console.log(error);
+
+
+       await manager.getProducts();
+
+        
+              let product3 = await manager.addProduct("FGA04", "CDJ-3000", "Negra", 3000000, "no-image", 29)
+        console.log(product3)
+         let productoId= await manager.getProductById(2)
+         console.log(productoId);
+     
+    } catch (error) {
+        console.log(error);
+    }
+
+
 }
-};
 operacionesProductos();
